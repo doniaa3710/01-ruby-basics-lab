0a306d971b6c07502e44c8ceb9e85a1fce6a111c
Donia Abdelhalim <doniaa3710@hstat.org>
2018-10-10 02:26:50 +0000

    fix argument

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,29 @@
-def division
+def division(num1=14, num2=2)
+    
+    num1/num2
+end
+
+def argue(argument)
+    return argument = "I'm right and you are wrong!"
     
 end
 
-def argue
-  
-end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    return "Nice"
 end
 
-def pizza_party
-    
+
+def pizza_party(topping = "cheese")
+    puts topping
+    argument = topping
 end
 
-def whisper
+def whisper(something)
+    return something.downcase
     
 end
